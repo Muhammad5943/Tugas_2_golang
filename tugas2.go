9c139d3b663e6569b7cb1b862768cbc18c6547f8
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-06-02 09:54:13 +0700

    Mengumpulkan tugas 2 golang

---
+++ tugas2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+  var angka = 51;
+
+  if angka % 2 == 0  {
+    fmt.Println("Bilangan Genap")
+  } else {
+    fmt.Println("Bilangan Ganjil")
+  }
+
+}
